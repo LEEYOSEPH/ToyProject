4265cfdfd2e048e71274763fa36b9f00d5d0f285
LEEYOSEPH <laksmi0628@gmail.com>
2022-09-24 08:32:31 +0900

    feat: 작성글 저장

--- jpaBoard/src/main/java/com/toyproject/jpaBoard/board/controller/PostController.java
+++ jpaBoard/src/main/java/com/toyproject/jpaBoard/board/controller/PostController.java
@@ -1,27 +1,26 @@
 package com.toyproject.jpaBoard.board.controller;
 
 import com.toyproject.jpaBoard.board.request.PostCreate;
+import com.toyproject.jpaBoard.board.service.PostService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-@RestController
 @Slf4j
+@RestController
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
     
     /*게시글 등록*/
     @PostMapping("/posts")
-    public Map<String,String> posts(@RequestBody @Valid PostCreate params) {
-
+    public Map<String,String> posts(@RequestBody @Valid PostCreate request) {
+        postService.write(request);
         return Map.of();
     }
 }
---
+++ jpaBoard/src/main/java/com/toyproject/jpaBoard/board/repository/Post.java
@@ -0,0 +1,23 @@
+package com.toyproject.jpaBoard.board.repository;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class Post {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Lob
+    private String content;
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
